df0e1f1ce547d59bab1368640de938c1c2ccbfae
João Pedro Rocha <jpcrocha8@gmail.com>
2023-05-16 15:09:22 -0300

    js começo

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,19 @@
-let arm1 = [];
+var scoreA = 0;
+var scoreB = 0;
+
+
+function addScore(quantidade, time) {
+    if (time == "A"){
+        scoreA += quantidade;
+
+        document.getElementById("pontosa").innerHTML = scoreA;
+    }
+    if (time == "B"){
+        scoreB += quantidade;
+
+        document.getElementById("pontosb").innerHTML = scoreB;
+    }
+
+}
+console.log(scoreA);
+console.log(scoreB);
